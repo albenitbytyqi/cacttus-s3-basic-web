efc9b17fa4fb42b21696b82693956d80432dd0bf
Albenit <35097523+Albenit@users.noreply.github.com>
2021-01-06 23:06:41 +0100

    Create register.php
    Ndertimi i UI per Register

---
+++ task-menagment/register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+    <center>
+        <h4>Task Menagment Tool - REGISTER</h4>
+        <form>
+            <label>Full Name:</label><br>
+            <input type="text" name="full_name"/><br>
+            <label>Email:</label><br>
+            <input type="email" name="email"/><br>
+            <label>Password:</label><br>
+            <input type="password" name="password"/>
+            <br><br>
+            <button>Register</button>
+
+        </form>
+    </center>
+</body>
+</html>
