869194315ac61d318ac11587aef0c8d0d92f01a2
Albenit <35097523+Albenit@users.noreply.github.com>
2021-01-06 22:54:13 +0100

    Update login.php
    Ndertimi i UI per login formen

--- task-menagment/login.php
+++ task-menagment/login.php
@@ -1,3 +1,19 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Log In</title>
+</head>
+<body>
+    <center>
+        <form>
+        <h4>Task Menagment Tool - LOGIN</h4>
+            <label>Email:</label><br>
+            <input type="email" name="email"/><br>
+            <label>Password:</label><br>
+            <input type="password" name="password"/>
+            <br><br>
+            <button>Login</button>
+        </form>
+    </center>
+</body>
+</html>
