406036ab116d981997ac7a226999adac03ce9ccf
Albenit <35097523+Albenit@users.noreply.github.com>
2021-01-07 00:40:07 +0100

    Create register_logic.php
    Ndertimi i logjikes te register

---
+++ task-menagment/register_logic.php
@@ -0,0 +1,18 @@
+<?php
+
+    require_once "processes.php";
+
+
+    $fullName = $_POST['full_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = [
+        'full_name' => $fullName,
+        'email' => $email,
+        'password' => $password
+    ];
+
+    storeUserToDb($user);
+    header("location: /cacttus-s3-basic-web/task-menagment/register.php");
+?>
