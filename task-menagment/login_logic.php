1626771cdc1050f24983f53f224b88089c4e12af
Albenit <35097523+Albenit@users.noreply.github.com>
2021-01-07 00:38:17 +0100

    Create login_logic.php
    Ndertimi i logjikes per loginin

---
+++ task-menagment/login_logic.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    require_once "processes.php";
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = checkUserByEmailAndPass($email,$password);
+
+    if($user != null){
+        // logged in
+        echo "Logged in!!!";
+        $_SESSION['logged_in'] = true;
+        $_SESSION['first_name'] = $user['first_name'];
+        $_SESSION['email'] = $email;
+        header("Location: /cacttus-s3-basic-web/task-menagment/add_task.php");
+        die();
+    }else {
+        echo "Wrong Email or Password!";
+        $_SESSION['logged_in'] = false;
+    }
+    
+    
+    ?>
