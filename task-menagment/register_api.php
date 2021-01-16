b43581f67628efd21f875e4d32b7e4bb130411e0
Albenit <35097523+Albenit@users.noreply.github.com>
2021-01-17 00:47:55 +0100

    Updeta register_api.php

--- task-menagment/register_api.php
+++ task-menagment/register_api.php
@@ -3,6 +3,24 @@
 
     require_once "processes.php";
 
+    // header('Content-Type: application/json');
+    
+    // if($_SERVER['REQUEST_METHOD'] != 'POST'){
+    //     echo json_encode([
+    //         'success' => false,
+    //         'message' => 'Post HTTP method required'
+    //     ]);
+    //     die();
+    // }
+
+    // if(isUserLoggedIn()){
+    //     echo json_encode([
+    //         'success' => false,
+    //         'message' => 'User is alredy authenticateed'
+    //     ]);
+    //     die();
+    // }
+
 
     $fullName = $_POST['full_name'];
     $email = $_POST['email'];
@@ -14,6 +32,27 @@
         'password' => $password
     ];
 
+    if(checkUserByEmail($email)){
+        echo "User is Already registred";
+        die();
+    }
+
+    // if (checkUserByEmail($email)) {
+    //     echo json_encode([
+    //         'success' => false,
+    //         'message' => "This User Is Alredy Registred!"
+    //     ]);
+    //     die();
+   // }
+
     storeUserToDb($user);
- //   header("location: /cacttus-s3-basic-web/task-menagment/register.php");
+
+    // if(storeUserToDb($user)){
+    //     echo json_encode([
+    //         'success' => true,
+    //         'message' => "Welcome"
+    //     ]);
+    //     die();
+    // }
+    header("location: /cacttus-s3-basic-web/task-menagment/register_ajax.php");
 ?>
