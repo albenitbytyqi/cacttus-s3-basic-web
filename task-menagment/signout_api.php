2a9834595bf0d5ba6bd2165c2e513f98556e5aba
Albenit <35097523+Albenit@users.noreply.github.com>
2021-01-17 00:48:19 +0100

    Update signout.php

--- task-menagment/signout_api.php
+++ task-menagment/signout_api.php
@@ -1,7 +1,18 @@
 <?php
     require_once "processes.php";
 
+    header('Content-Type: application/json');
+    
+    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+        echo json_encode([
+            'success' => false,
+            'message' => 'POST HTTP Method required!'
+        ]);
+        die();
+    }
+
     signOut();
+    
     echo json_encode([
         'success' => true,
         'message' => "User is Signout"
