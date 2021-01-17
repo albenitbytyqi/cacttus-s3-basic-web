3dd329d104e739bf96cd7808c0a2791198fc26e5
Albenit <35097523+Albenit@users.noreply.github.com>
2021-01-18 00:50:00 +0100

    update add_task.php

--- task-menagment/add_task.php
+++ task-menagment/add_task.php
@@ -18,19 +18,21 @@
     <body>
 
         <center>
-            <form>
+            <form method="POST" action="/cacttus-s3-basic-web/task-menagment/add_task_logic.php">
                 <h2>Add New Task:</h2><br>
                 <label>Title:</label><br>
                 <input type="text" name="title" size="52"/><br>
                 <label>Description:</label><br>
                 <textarea rows="5" cols="40" name="description"></textarea><br><br>
-                Status: <select >
+                Status: <select name="status">
                 <option>ToDo</option>
                 <option>InProgress</option>
                 <option>Done</option>
                 </select><br><br>
                 <input type="submit" value="Submit"/>
             </form>
+
+
         </center>
 
     </body>
